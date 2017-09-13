a3da9d0edcf746a4a9acab88e2d3e49cde3cc908
Théo Zimmermann <theo.zimmermann@univ-paris-diderot.fr>
2017-09-13 11:06:41 +0200

    Load input script from file.

--- test/test.ml
+++ test/test.ml
@@ -1,9 +1,9 @@
 (* ocamlbuild -use-ocamlfind -j 4 -pkgs 'sexplib,dynlink,camlp5.gramlib,coq.ltac,coq.stm,coq.idetop' -tag 'linkall' -I sertop test/test.native && ./test.native *)
 
-open Helpers
+let input_script filename =
+  Core_kernel.Std.In_channel.read_lines filename
+  |> String.concat "\n"
 
-let input_script =
-  "Goal forall n m, n + m = m + n. induction n. (* comment *) all: cycle 1. all: cycle 1. { easy. } idtac. Abort."
 
 let print_sentence = function
   | (sentence, None) ->
@@ -15,5 +15,9 @@ let print_sentence = function
 
 
 let _ =
-  script_with_goal_info input_script
-  |> List.iter print_sentence
+  if Array.length Sys.argv > 1 then
+    input_script Sys.argv.(1)
+    |> Helpers.script_with_goal_info
+    |> List.iter print_sentence
+  else
+    exit 1
